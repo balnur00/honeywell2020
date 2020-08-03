f2194ac77b3ea0a76412ab8921a028184eaabe8a
unknown <balnur00@mail.ru>
2020-08-03 17:54:20 +0600

    db app sql server version 1

---
+++ ZadanieWPF2/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace ZadanieWPF2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        string connectionString;
+        SqlDataAdapter adapter;
+        DataTable employeesTable;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // получаем строку подключения из app.config
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string sql = "SELECT * FROM Employees";
+            employeesTable = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
+
+                // установка команды на добавление для вызова хранимой процедуры
+                adapter.InsertCommand = new SqlCommand("sp_InsertEmployee", connection);
+                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@fullname", SqlDbType.NVarChar, 255, "Full Name"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@age", SqlDbType.Int, 0, "Age"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@position", SqlDbType.NVarChar, 50, "Position"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@gender", SqlDbType.NVarChar, 50, "Gender"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@married", SqlDbType.Bit, 0, "Position"));
+                SqlParameter parameter = adapter.InsertCommand.Parameters.Add("@Id", SqlDbType.Int, 0, "Id");
+                parameter.Direction = ParameterDirection.Output;
+
+                connection.Open();
+                adapter.Fill(employeesTable);
+                employeesGrid.ItemsSource = employeesTable.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+
+        private void UpdateDB()
+        {
+            SqlCommandBuilder comandbuilder = new SqlCommandBuilder(adapter);
+            adapter.Update(employeesTable);
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateDB();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (employeesGrid.SelectedItems != null)
+            {
+                for (int i = 0; i < employeesGrid.SelectedItems.Count; i++)
+                {
+                    DataRowView datarowView = employeesGrid.SelectedItems[i] as DataRowView;
+                    if (datarowView != null)
+                    {
+                        DataRow dataRow = (DataRow)datarowView.Row;
+                        dataRow.Delete();
+                    }
+                }
+            }
+            UpdateDB();
+        }
+    }
+}
